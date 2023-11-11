16495cc9b4829f2f04ea4a4544815d0d03d7b78a
PhilippePerret <philippe.perret@yahoo.fr>
2023-11-11 16:28:46 +0100

    Ajout de 'page'

--- Manuel/manuel_building/Features/recette/recette_collection.rb
+++ Manuel/manuel_building/Features/recette/recette_collection.rb
@@ -4,7 +4,7 @@ Prawn4book::Manual::Feature.new do
   subtitle "Recette de la collection"
 
   description <<~EOT
-    La *recette de la collection* est un fichier de nom `recipe_collection.yaml` qui se trouve à la racine du dossier d’une collection de livres. Son nom vient de "recipe" qui signifie *recette* en anglais et de `.yaml`, extension des fichiers au format simple `YAML` (cf. [[annexe/format_yaml]]).
+    La *recette de la collection* est un fichier de nom `recipe_collection.yaml` qui se trouve à la racine du dossier d’une collection de livres. Son nom vient de "recipe" qui signifie *recette* en anglais et de `.yaml`, extension des fichiers au format simple `YAML` (cf. page [[annexe/format_yaml]]).
     Vous pouvez trouver ci-dessous les données propres à une collection. 
     EOT
 
--- Manuel/manuel_building/Features/recette/recette_livre.rb
+++ Manuel/manuel_building/Features/recette/recette_livre.rb
@@ -3,7 +3,7 @@ Prawn4book::Manual::Feature.new do
   subtitle "Recette du livre"
 
   description <<~EOT
-    La *recette du livre* du livre est un fichier de nom `recipe.yaml` qui se trouve à la racine du dossier du livre. Son nom vient de "recipe" qui signifie *recette* en anglais et de `.yaml`, extension des fichiers au format simple `YAML` (cf. [[annexe/format_yaml]]).
+    La *recette du livre* du livre est un fichier de nom `recipe.yaml` qui se trouve à la racine du dossier du livre. Son nom vient de "recipe" qui signifie *recette* en anglais et de `.yaml`, extension des fichiers au format simple `YAML` (cf. page [[annexe/format_yaml]]).
     EOT
 
 
