8aca60ff64e5abcd08a8316796f212e8afaf0567
PhilippePerret <philippe.perret@yahoo.fr>
2023-12-22 05:57:29 +0100

    Minor change

--- Manuel/manuel_building/Features/expert/bibliographies.rb
+++ Manuel/manuel_building/Features/expert/bibliographies.rb
@@ -4,9 +4,9 @@ Prawn4book::Manual::Feature.new do
 
 
   description <<~EOT
-    TODO: Décrire comment faire une méthode de formatage propre dans `Prawn4book::Bibliography’ (méthode d’instance) pour l’utiliser dans la liste des sources, pour une propriété particulières. Si, par exemple, la donnée `format’ de la bibliographie (dans la recette), définit `%{title|mon_transformeur}`, alors il faut définir la méthode `Prawn4book::Bibliography#mon_tranformeur’ qui reçoit en argument la valeur de :title de l’item.
+    TODO: Décrire comment faire une méthode de formatage propre dans `Prawn4book::Bibliography` (méthode d’instance) pour l’utiliser dans la liste des sources, pour une propriété particulières. Si, par exemple, la donnée `format` de la bibliographie (dans la recette), définit `%{title|mon_transformeur}`, alors il faut définir la méthode `Prawn4book::Bibliography#mon_tranformeur` qui reçoit en argument la valeur de :title de l’item.
 
-    TODO: Montrer qu’on peut par exemple définir une font et/ou une couleur propre comme pour la bibliographie `costume’
+    TODO: Montrer qu’on peut par exemple définir une font et/ou une couleur propre comme pour la bibliographie `costume`
     EOT
 
   # sample_texte <<~EOT #, "Autre entête"
